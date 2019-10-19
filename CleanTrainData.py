ed239c747829984f8af11eb87873a8bbc874b37a
Janina Mothes <janinamothes@web.de>
2019-10-19 12:13:51 +0200

    Comments added

--- CleanTrainData.py
+++ CleanTrainData.py
@@ -10,18 +10,18 @@ df = pd.merge(store, train, on='Store')
 df = hlp.date_convert(df)
 
 # Data Cleaning
-df = hlp.CompYear(df)
-df = hlp.CompAct(df)
-df = hlp.PromoDur(df)
-df = hlp.RunAnyPromo(df)
-df = hlp.RunPromo(df)
-df = hlp.CustImput(df)
+df = hlp.CompYear(df)       # Converts CompetitionYear and CompetitionMonth to datetime format
+df = hlp.CompAct(df)        # Calculate if Competition is active on the Date and how long the competition is active
+df = hlp.PromoDur(df)       # Create additional column: 'PromoDuration' -> how long is the Promotion running
+df = hlp.RunAnyPromo(df)    # Create additional column: 'RunningAnyPromo' -> binary column representing if Promo1 or Promo2 is running on the Date
+df = hlp.RunPromo(df)       # Create additional column: 'RunPromo' -> binary column representing if Promo2 is running on the Date
+df = hlp.CustImput(df)      # Data imputation of the 'Customer' column: if Store is open, Customer is set to mean(Customer) else 0
 
 df.drop({'CompetitionStart', 'CompetitionOpenSinceYear', 'CompetitionOpenSinceMonth', 'PromoStart','PromoInterval','Promo','Promo2', 'Promo2SinceYear', 'Promo2SinceWeek', 'DayOfWeek'}, axis=1, inplace=True)
 df.dropna(axis=0, how='any', subset=['Sales', 'Open', 'StateHoliday', 'SchoolHoliday','CompetitionDistance'], inplace=True)
 df['CompetitionDays'].fillna(0, inplace=True)
 
-df = hlp.MeanSales(df, type='Train')
-df = hlp.onehotencoding(df)
+df = hlp.MeanSales(df, type='Train')    # Create new columns: Rel (Relative Sales), ExpectedSales
+df = hlp.onehotencoding(df)             # one hot encoding
 
 df.to_csv('data/CleanTrainData_ohe.csv')
