ac88c46f97e7e500422e4364396f8b99b0fd6f62
Janina Mothes <janinamothes@web.de>
2019-10-31 14:46:54 +0100

    Code cleaned

--- Run_Model.py
+++ Run_Model.py
@@ -1,12 +1,12 @@
-import helper as hlp
-import pandas as pd
-import xgboost as xgb
 from datetime import timedelta
-from bayes_opt import BayesianOptimization
 import pickle
+import pandas as pd
+import helper as hlp
+from bayes_opt import BayesianOptimization
+import xgboost as xgb
 
 ################# Loading the cleaned Training Data ################################
-df = pd.read_csv('data/CleanTrainData_ohe.csv',index_col=0)
+df = pd.read_csv('data/CleanTrainData_ohe.csv', index_col=0)
 df = df.loc[df.Sales != 0]
 
 ################# Splitting the data in Train and Validation Sets ################################
@@ -15,12 +15,12 @@ df.loc[:, 'Date'] = pd.to_datetime(df['Date'])
 cols = list(df.columns.values)
 cols.pop(cols.index('Sales'))
 df = df[cols + ['Sales']]
-X, y = df.iloc[:, :-1],df.iloc[:, -1]
+X, y = df.iloc[:, :-1], df.iloc[:, -1]
 
 # Split the data into Training and Validation Data based on the time
-date_range_days=(df.Date.max() - df.Date.min()).days
-split_date=df.Date.min() + timedelta(date_range_days*0.8)
-df_early,df_later = df.loc[df.Date <= split_date], df.loc[df.Date > split_date]
+date_range_days = (df.Date.max() - df.Date.min()).days
+split_date = df.Date.min() + timedelta(date_range_days*0.8)
+df_early, df_later = df.loc[df.Date <= split_date], df.loc[df.Date > split_date]
 
 ################# Remove Datetime-Format Columns ################################
 # Remove Date-Column, since Datetime-Format is not accepted by XGBoost
@@ -28,16 +28,17 @@ df_early.drop({'Date'}, axis=1, inplace=True)
 df_later.drop({'Date'}, axis=1, inplace=True)
 
 ################# Create X-FeatureMatrix and Y-PredictorVector ################################
-X_train, X_val, y_train, y_val = df_early.iloc[:,:-1], df_later.iloc[:,:-1], df_early.iloc[:,-1], df_later.iloc[:,-1]
+X_train, X_val, y_train, y_val = \
+    df_early.iloc[:, :-1], df_later.iloc[:, :-1], df_early.iloc[:, -1], df_later.iloc[:, -1]
 
 ################# Defining the hyper parameter ################################
 params = {
-# Learning Task Parameters
+    # Learning Task Parameters
     'objective': 'reg:squarederror',
     'eval_metric':'rmse', # Evaluation metrics for validation data
-# Parameters for Tree Booster
+    # Parameters for Tree Booster
     'learning_rate': 0.05, # Learning Rate: step size shrinkage used to prevent overfitting.
-# Paramters for XGB ScikitLearn API
+    # Paramters for XGB ScikitLearn API
     'n_jobs': 4, # Number of parallel threads used to run xgboost
     'n_estimators': 1000, # number of trees you want to build
     'verbosity': 2, # degree of verbosity: 0 (silent) - 3 (debug)
@@ -49,10 +50,10 @@ params = {
 
 ################# Defining the fit parameters ################################
 fit_params = {
-            'eval_metric':'rmse',
-            'early_stopping_rounds': 10,
-            'eval_set': [(X_val, y_val)],
-            }
+    'eval_metric':'rmse',
+    'early_stopping_rounds': 10,
+    'eval_set': [(X_val, y_val)],
+}
 
 ################# Initiate XGB regressor and run initial fit ################################
 xgb_reg = xgb.XGBRegressor(**params)
@@ -65,32 +66,31 @@ df_DM = xgb.DMatrix(data=X_train, label=y_train)
 def xgb_evaluate(max_depth, reg_lambda, colsample_bytree, subsample):
     params1 = {
         'colsample_bytree': colsample_bytree,
-        'max_depth': int(round(max_depth)),  # Maximum depth of a tree: high value -> prone to overfitting
+        'max_depth': int(round(max_depth)),  # Maximum depth of a tree: high value
+                                             # -> prone to overfitting
         'reg_lambda': reg_lambda,  # L2 regularization term on weights
         'subsample': subsample
     }
     cv_result = xgb.cv(dtrain=df_DM,
-                        params=params1,
-                        early_stopping_rounds=10,
-                        num_boost_round=100,
-                        metrics='rmse')
+                       params=params1,
+                       early_stopping_rounds=10,
+                       num_boost_round=100,
+                       metrics='rmse')
     return -cv_result['test-rmse-mean'].iloc[-1]
 
 ################# Using BayesOptimization for evaluation function ################################
 optimizer = BayesianOptimization(xgb_evaluate, {'max_depth': (3, 5),
-                                                    'reg_lambda': (0, 5),
-                                                    'colsample_bytree': (0.3, 0.8),
-                                                    'subsample': (0.8, 1)})
+                                                'reg_lambda': (0, 5),
+                                                'colsample_bytree': (0.3, 0.8),
+                                                'subsample': (0.8, 1)})
 
 ################# calculate maximum of Bayes optimization ################################
 optimizer.maximize(init_points=10, n_iter=10)
-# n_iter: How many steps of bayesian optimization you want to perform. The more steps the more likely to find a good maximum you are.
-# init_points: How many steps of random exploration you want to perform. Random exploration can help by diversifying the exploration space.
 
 ################# retrieving optimized parameters ################################
-params1 = optimizer.max['params']
-params1['max_depth']= int(round(params1['max_depth']))
-params.update(params1)
+params_1 = optimizer.max['params']
+params_1['max_depth'] = int(round(params_1['max_depth']))
+params.update(params_1)
 
 ################# Run model with optimized parameters ################################
 xgb_reg = xgb.XGBRegressor(**params)
@@ -100,7 +100,7 @@ xgb_reg.fit(X_train, y_train, **fit_params)
 train_preds = xgb_reg.predict(X_train)
 val_preds = xgb_reg.predict(X_val)
 
-################# Print metrics for Train and Validation set prediction ################################
+################# Print metrics for Train and Validation set prediction ########################
 print("RMSE train: %f" % hlp.rmspe(y_train, train_preds))
 print("RMSE CV: %f" % hlp.rmspe(y_val, val_preds))
 print("Adam's metric (train): %f" %(hlp.adam_metric(y_train, train_preds)))
--- helper.py
+++ helper.py
@@ -3,10 +3,12 @@ import numpy as np
 import pandas as pd
 import datetime as dt
 
-# Converts 'Date'-column to datetime format and creates extra columns: Year, Month, Week, Weekday, Day
+# Converts 'Date'-column to datetime format and creates extra columns:
+# Year, Month, Week, Weekday, Day
 def date_convert(df):
     '''
-    Function converts the entries of a 'Date' column to datetime format and creates separate columns for: year, month, week, weekday, day.
+    Function converts the entries of a 'Date' column to datetime format and
+    creates separate columns for: year, month, week, weekday, day.
     :param pandas.dataframe:
     :return pandas.datafram:
     '''
@@ -32,13 +34,17 @@ def date_convert(df):
 # Converts CompetitionYear and CompetitionMonth to datetime format
 def CompYear(df):
     '''
-    Function combines 'CompetitionYear' and 'CompetitionMonth to new column 'CompetitionStart' in datetime format
+    Function combines 'CompetitionYear' and 'CompetitionMonth
+    to new column 'CompetitionStart' in datetime format
     :param pandas.dataframe:
     :return pandas.dataframe:
     '''
     df['CompetitionStart'] = 'NaT'
     mask = (~df['CompetitionOpenSinceYear'].isnull()) & (~df['CompetitionOpenSinceMonth'].isnull())
-    df['CompetitionStart'] = df.loc[mask, 'CompetitionOpenSinceYear'].astype(int).astype(str) + '-' + df.loc[mask, 'CompetitionOpenSinceMonth'].astype(int).astype(str) + '-01'
+    df['CompetitionStart'] = df.loc[mask, 'CompetitionOpenSinceYear'].astype(int).astype(str) \
+                             + '-' \
+                             + df.loc[mask, 'CompetitionOpenSinceMonth'].astype(int).astype(str) \
+                             + '-01'
     df['CompetitionStart'] = pd.to_datetime(df['CompetitionStart'])
     return df
 
@@ -47,35 +53,40 @@ def CompAct(df):
     df['CompetitionActive'] = 0
     df['CompetitionDays'] = 0
     df.loc[df['CompetitionStart'] <= df['Date'], 'CompetitionActive'] = 1
-    df['CompetitionDays'] = (df['Date'] - df['CompetitionStart'])/np.timedelta64(1,'D')
+    df['CompetitionDays'] = (df['Date'] - df['CompetitionStart'])/np.timedelta64(1, 'D')
     return df
 
 # Create additional column: 'PromoDuration' -> how long is the Promotion running
 def PromoDur(df):
     # Convert Promoyear and Promoweekno to datetime format
-    df_subset = df.loc[(~df['Promo2SinceYear'].isnull()) & (~df['Promo2SinceWeek'].isnull()), ['Promo2SinceYear','Promo2SinceWeek']]
-    df_subset = df_subset[['Promo2SinceYear','Promo2SinceWeek']].astype(int)
-    df['PromoStart'] = df_subset.apply(lambda row: dt.datetime.strptime(f'{row.Promo2SinceYear} {row.Promo2SinceWeek} 1', '%G %V %u'), axis=1)
+    df_subset = df.loc[(~df['Promo2SinceYear'].isnull()) & (~df['Promo2SinceWeek'].isnull()), \
+                       ['Promo2SinceYear', 'Promo2SinceWeek']]
+    df_subset = df_subset[['Promo2SinceYear', 'Promo2SinceWeek']].astype(int)
+    df['PromoStart'] = df_subset.apply(lambda row: dt.datetime.strptime(f'{row.Promo2SinceYear} \
+{row.Promo2SinceWeek} 1', '%G %V %u'), axis=1)
 
     # create PromoDuration Column:  Date - PromoStart
-    df['PromoDuration'] = (df['Date'] - df['PromoStart'])/np.timedelta64(1,'D')
+    df['PromoDuration'] = (df['Date'] - df['PromoStart'])/np.timedelta64(1, 'D')
     df['PromoDuration'].fillna(0, inplace=True)
     return df
 
-# Create additional column: 'RunningAnyPromo' -> binary column representing if Promo1 or Promo2 is running on the Date
+# Create additional column: 'RunningAnyPromo'
+# -> binary column representing if Promo1 or Promo2 is running on the Date
 def RunAnyPromo(df):
     df['RunningAnyPromo'] = 0
     months_abbr = []
 
-    for i in range(1,13):
+    for i in range(1, 13):
         months_abbr.append((i, date(2008, i, 1).strftime('%b')))
 
     for i in months_abbr:
-        mask = (df['PromoInterval'].str.contains(i[1], na=False)) & (df['Month']==i[0]) & (df['Promo2']==1) | df['Promo']==1
+        mask = (df['PromoInterval'].str.contains(i[1], na=False)) & (df['Month'] == i[0]) & \
+               (df['Promo2'] == 1) | df['Promo'] == 1
         df.loc[mask, 'RunningAnyPromo'] = 1
     return df
 
-# Create additional column: 'RunPromo' -> binary column representing if Promo2 is running on the Date
+# Create additional column: 'RunPromo'
+# -> binary column representing if Promo2 is running on the Date
 def RunPromo(df):
     # Sets RunningPromo to 1 if Months in Substring of PromoIntervall and current month match
     df['RunningPromo2'] = 0
@@ -84,11 +95,13 @@ def RunPromo(df):
         months_abbr.append((i, date(2008, i, 1).strftime('%b')))
 
     for i in months_abbr:
-        mask = (df['PromoInterval'].str.contains(i[1], na=False)) & (df['Month'] == i[0]) & (df['Promo2'] == 1)
+        mask = (df['PromoInterval'].str.contains(i[1], na=False)) & \
+               (df['Month'] == i[0]) & (df['Promo2'] == 1)
         df.loc[mask, 'RunningPromo2'] = 1
     return df
 
-# Data imputation of the 'Customer' column: if Store is open, Customer is set to mean(Customer) else 0
+# Data imputation of the 'Customer' column:
+# if Store is open, Customer is set to mean(Customer) else 0
 def CustImput(df):
     # Replace NaN in Customers with Mean(Customers), but if Store not open set Customers to 0
     df['Customers'].fillna(df['Customers'].mean(), inplace=True)
@@ -103,7 +116,8 @@ def MeanSales(df, type='Train'):
     df['ExpectedSales'] = np.nan
     if type == 'Train':
         # Calculate the mean Sales dependent on the StoreInfo normalized by the Customers
-        mean_sales = df.loc[df.Sales > 0, ['Sales', 'Customers', 'StoreInfo']].groupby('StoreInfo').mean()
+        mean_sales = df.loc[df.Sales > 0, ['Sales', 'Customers', 'StoreInfo']]\
+            .groupby('StoreInfo').mean()
         mean_sales['Rel'] = mean_sales['Sales'] / mean_sales['Customers']
         b = mean_sales['Rel'].to_dict()
         df['Rel'] = df['StoreInfo'].map(b)
@@ -114,7 +128,8 @@ def MeanSales(df, type='Train'):
         with open('traindata/global_sales.txt', 'w') as f:
             f.write(str(global_sales))
     else:
-        # in the case of the Test set, there will be no Sales column available -> the relative sales are retrieved from the training set
+        # in the case of the Test set, there will be no Sales column available
+        # -> the relative sales are retrieved from the training set
         b = pd.read_csv('traindata/MeanSales.csv', header=None, names=['keys', 'values'])
         mydict = dict(zip(b['keys'], b['values']))
         df['Rel'] = df['StoreInfo'].map(mydict)
